fcde9a8a8a5b597829e60243bb22a321c0cb4e95
thisoldbear <danielwilliams.design@gmail.com>
2019-05-07 21:36:58 +0100

    convert class to functional component

--- src/map-three/map.js
+++ src/map-three/map.js
@@ -1,84 +1,47 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useRef, useState, useEffect } from 'react';
 import newGoogleMap from '../utils/newGoogleMap';
 import renderMarkers from '../utils/renderMarkers';
 
-class Map extends Component {
-  // Initial state
-  state = {
-    mapReady: false,
+const Map = ({ options }) => {
+  const mapEl = useRef(null);
+  const mapObj = useRef(null);
 
-    // Map markers
-    markers: [{
-      title: 'Daniel',
-      id: '1',
-      location: {
-        lat: '51.45000',
-        lng: '-2.55000'
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    mapObj.current = newGoogleMap(mapEl.current, options);
+
+    setMarkers([
+      {
+        title: 'Daniel',
+        id: '1',
+        location: {
+          lat: '51.45000',
+          lng: '-2.55000'
+        }
       }
-    },
-    {
-      title: 'David',
-      id: '2',
-      location: {
-        lat: '51.46000',
-        lng: '-2.56000'
-      }
-    }]
-  }
-
-  componentDidMount() {
-    this.map = newGoogleMap(ReactDOM.findDOMNode(this.refs.map), this.props.options);
-
-    this.setState({
-      mapReady: true,
-    });
+    ]);
 
     // Mock state change over time
     setTimeout(() => {
-      this.setState({
-        markers: [
-          ...this.state.markers,
-          {
-            title: 'Colin',
-            id: '3',
-            location: {
-              lat: '51.43000',
-              lng: '-2.53000'
-            }
+      setMarkers([
+        {
+          title: 'David',
+          id: '2',
+          location: {
+            lat: '51.46000',
+            lng: '-2.56000'
           }
-        ]
-      })
+        }
+      ]);
     }, 4000);
+  }, [options]);
 
-    setTimeout(() => {
-      this.setState({
-        markers: [
-          {
-            title: 'Nigel',
-            id: '4',
-            location: {
-              lat: '51.42000',
-              lng: '-2.57000'
-            }
-          }
-        ]
-      })
-    }, 6000);
-  }
-
-  render() {
-    const {
-      mapReady,
-      markers,
-    } = this.state;
-
-    return (
-      <div className="map" ref="map">
-        {mapReady && renderMarkers(markers, this.map)}
-      </div>
-    );
-  }
+  return (
+    <div className="map" ref={mapEl}>
+      {renderMarkers(markers, mapObj.current)}
+    </div>
+  );
 }
 
 export default Map;
